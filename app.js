4c88ef5e5a7dab6308b9441fc3f44d8303671def
peterfchris <peter.f.chris@gmail.com>
2020-11-11 15:07:09 -0700

    Type Conversion Notes

--- app.js
+++ app.js
@@ -49,26 +49,68 @@
 // Primitive Types
 
 // String
-const name = 'John Doe'
-// Number
-const age = 45
-// Boolean
-const hasKids = true
-// Null
-const car = null
-// Undefined
-let test;
-// Symbol
-const sym = Symbol()
+// const name = 'John Doe'
+// // Number
+// const age = 45
+// // Boolean
+// const hasKids = true
+// // Null
+// const car = null
+// // Undefined
+// let test;
+// // Symbol
+// const sym = Symbol()
 
-// Reference Types
-// Array
-const hobbies = ['movies', 'music']
-// Object Literal
-const address = {
-  city: 'Boston',
-  state: 'MA'
-}
-const today = new Date()
-console.log('today', today)
-console.log(typeof today)
+// // Reference Types
+// // Array
+// const hobbies = ['movies', 'music']
+// // Object Literal
+// const address = {
+//   city: 'Boston',
+//   state: 'MA'
+// }
+// const today = new Date()
+// console.log('today', today)
+// console.log(typeof today)
+
+let val;
+
+// Number to string
+
+val = String(555);
+val = String(4+4);
+//Bool to String
+val = String(true)
+// Date to string
+val=String(new Date())
+// Array to string
+val=String([1,2,3])
+
+// toString()
+val = (5).toString()
+val=(true).toString()
+
+// String to number
+val = Number('5')
+val = Number(true)
+val = Number(false)
+val = Number(null)
+val = Number('hello')
+val = Number([1,2,3])
+
+val = parseInt('100')
+val = parseFloat('100.30')
+
+
+// Output
+// console.log(val)
+// console.log(typeof val)
+// // console.log(val.length)
+// console.log(val.toFixed(2))
+
+const val1 = String(5)
+const val2 = 6
+const sum = Number(val1 + val2)
+
+console.log(sum)
+console.log(typeof sum)

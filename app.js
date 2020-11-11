355ef31305396f10ec3bcb569af845bc3b3c1450
peterfchris <peter.f.chris@gmail.com>
2020-11-11 15:18:25 -0700

    Numbers and Math Object notes

--- app.js
+++ app.js
@@ -73,44 +73,89 @@
 // console.log('today', today)
 // console.log(typeof today)
 
+// let val;
+
+// // Number to string
+
+// val = String(555);
+// val = String(4+4);
+// //Bool to String
+// val = String(true)
+// // Date to string
+// val=String(new Date())
+// // Array to string
+// val=String([1,2,3])
+
+// // toString()
+// val = (5).toString()
+// val=(true).toString()
+
+// // String to number
+// val = Number('5')
+// val = Number(true)
+// val = Number(false)
+// val = Number(null)
+// val = Number('hello')
+// val = Number([1,2,3])
+
+// val = parseInt('100')
+// val = parseFloat('100.30')
+
+
+// // Output
+// // console.log(val)
+// // console.log(typeof val)
+// // // console.log(val.length)
+// // console.log(val.toFixed(2))
+
+// const val1 = String(5)
+// const val2 = 6
+// const sum = Number(val1 + val2)
+
+// console.log(sum)
+// console.log(typeof sum)
+
+// Numbers and Math Notes
+const num1 = 100;
+const num2 = 50;
+
 let val;
 
-// Number to string
+// addition
+val = num1 + num2;
+// multiplication
+val = num1 * num2;
+// subraction
+val = num1 - num2;
+// division
+val = num1 / num2;
+// modulus operater - what's left over. This would give us 0
+val = num1 % num2;
 
-val = String(555);
-val = String(4+4);
-//Bool to String
-val = String(true)
-// Date to string
-val=String(new Date())
-// Array to string
-val=String([1,2,3])
+// Math Object
 
-// toString()
-val = (5).toString()
-val=(true).toString()
+// Gives us PI
+val = Math.PI;
+// Gives us Eulers Number
+val = Math.E;
+// Rounds the number normally. This would output 2
+val = Math.round(2.4);
+// Rounds up. This would give us 3
+val = Math.ceil(2.4);
+// Rounds down. This would give us 2
+val = Math.floor(2.8);
+// Square root
+val = Math.sqrt(64);
+// absolute number. This would give us 3
+val = Math.abs(-3);
+// Power. This will give us 8 to the power of 2, or 64
+val = Math.pow(8, 2);
+// Minimum number, this will give us -2
+val = Math.min(2,33,4,1,55,6,3,-2);
+// Max number - 55
+val = Math.max(2,33,4,1,55,6,3,-2);
+// Random decimal point. To get a random number we need to do some finagling
+val = Math.random();
 
-// String to number
-val = Number('5')
-val = Number(true)
-val = Number(false)
-val = Number(null)
-val = Number('hello')
-val = Number([1,2,3])
-
-val = parseInt('100')
-val = parseFloat('100.30')
-
-
-// Output
-// console.log(val)
-// console.log(typeof val)
-// // console.log(val.length)
-// console.log(val.toFixed(2))
-
-const val1 = String(5)
-const val2 = 6
-const sum = Number(val1 + val2)
-
-console.log(sum)
-console.log(typeof sum)
+// How to get a random number between 1 and 20
+val = Math.floor(Math.random() * 20 + 1) //The +1 makes it so that you don't get 0

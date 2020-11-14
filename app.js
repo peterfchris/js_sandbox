deed140369ce1afb1582632229f1605ef9c752f5
peterfchris <peter.f.chris@gmail.com>
2020-11-14 07:49:57 -0700

    Array Notes

--- app.js
+++ app.js
@@ -238,37 +238,104 @@
 
 
 // Template Literals Notes
- const name = 'John'
- const age = 30
- const job = 'Web Developer'
- const city = 'Miami'
- let html
+//  const name = 'John'
+//  const age = 30
+//  const job = 'Web Developer'
+//  const city = 'Miami'
+//  let html
 
-//  Without template strings (ES5)
-html = '<ul><li>Name: ' + name + ' </li><li>Age: ' + age + ' </li><li>Job: ' + job + ' </li><li>City: ' + city +' </li></ul>'
+// //  Without template strings (ES5)
+// html = '<ul><li>Name: ' + name + ' </li><li>Age: ' + age + ' </li><li>Job: ' + job + ' </li><li>City: ' + city +' </li></ul>'
 
-html = '<ul>' + 
-        '<li>Name: ' + name + '</li>' +
-        '<li>Age: ' + age + '</li>' +
-        '<li>Job: ' + job + '</li>' +
-        '<li>City: ' + city + '</li>' +
-        '</ul>'
+// html = '<ul>' + 
+//         '<li>Name: ' + name + '</li>' +
+//         '<li>Age: ' + age + '</li>' +
+//         '<li>Job: ' + job + '</li>' +
+//         '<li>City: ' + city + '</li>' +
+//         '</ul>'
 
-function hello() {
-  return 'hello'
+// function hello() {
+//   return 'hello'
+// }
+
+// // With template strings (es6)
+// html = `
+//     <ul>
+//       <li>Name: ${name}</li>
+//       <li>Age: ${age}</li>
+//       <li>Job: ${job}</li>
+//       <li>City: ${city}</li>
+//       <li>${2 + 2}</li>
+//       <li>${hello()}</li>
+//       <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+//     </ul>
+// `
+
+// document.body.innerHTML = html
+
+// ARRAY NOTES
+
+const numbers = [43,56,33,23,44,36,5]
+const numbers2 = new Array(22,45,33,76,54)
+const fruit = ['Apples', 'Banana', 'Orange', 'Pear']
+const mixed = [22,'Hello', true, undefined, null, {a:1, b:1}, new Date()]
+
+let val 
+
+// Get array length
+val = numbers.length
+// Check if is array
+val = Array.isArray(numbers)
+// Get single value
+val = numbers[3]
+val = numbers[0]
+// Insert into Array
+numbers[2] = 100
+// Find index of value
+val = numbers.indexOf(36)
+
+// MUTATING ARRAYS
+// Add on to end
+numbers.push(250)
+// Add to front
+numbers.unshift(120)
+// Take off from end
+numbers.pop()
+// Take off from front
+numbers.shift()
+// Splice values
+numbers.splice(1,3)
+// Reverse
+numbers.reverse()
+
+// Concatenate array
+val = numbers.concat(numbers2)
+
+// Sorting arrays
+val = fruit.sort()
+// sorting numbers sorts by first digit only
+val = numbers.sort()
+
+// Use the 'compare function' - sorts by whole number value
+val = numbers.sort(function(x,y){
+  return x-y
+})
+
+// Reverse Sort
+val = numbers.sort(function(x,y){
+  return y-x
+})
+
+// Find 
+function under50(num){
+  return num < 50
 }
 
-// With template strings (es6)
-html = `
-    <ul>
-      <li>Name: ${name}</li>
-      <li>Age: ${age}</li>
-      <li>Job: ${job}</li>
-      <li>City: ${city}</li>
-      <li>${2 + 2}</li>
-      <li>${hello()}</li>
-      <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
-    </ul>
-`
+function over50(num){
+  return num > 50
+}
 
-document.body.innerHTML = html
+val = numbers.find(over50)
+
+console.log(numbers)
+console.log(val)

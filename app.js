750e31ce4e0cd8e94d16333cb1a50c2b80251f2a
peterfchris <peter.f.chris@gmail.com>
2020-11-16 07:50:53 -0700

    Obj literal notes

--- app.js
+++ app.js
@@ -275,67 +275,109 @@
 
 // ARRAY NOTES
 
-const numbers = [43,56,33,23,44,36,5]
-const numbers2 = new Array(22,45,33,76,54)
-const fruit = ['Apples', 'Banana', 'Orange', 'Pear']
-const mixed = [22,'Hello', true, undefined, null, {a:1, b:1}, new Date()]
+// const numbers = [43,56,33,23,44,36,5]
+// const numbers2 = new Array(22,45,33,76,54)
+// const fruit = ['Apples', 'Banana', 'Orange', 'Pear']
+// const mixed = [22,'Hello', true, undefined, null, {a:1, b:1}, new Date()]
+
+// let val 
+
+// // Get array length
+// val = numbers.length
+// // Check if is array
+// val = Array.isArray(numbers)
+// // Get single value
+// val = numbers[3]
+// val = numbers[0]
+// // Insert into Array
+// numbers[2] = 100
+// // Find index of value
+// val = numbers.indexOf(36)
+
+// // MUTATING ARRAYS
+// // Add on to end
+// numbers.push(250)
+// // Add to front
+// numbers.unshift(120)
+// // Take off from end
+// numbers.pop()
+// // Take off from front
+// numbers.shift()
+// // Splice values
+// numbers.splice(1,3)
+// // Reverse
+// numbers.reverse()
+
+// // Concatenate array
+// val = numbers.concat(numbers2)
+
+// // Sorting arrays
+// val = fruit.sort()
+// // sorting numbers sorts by first digit only
+// val = numbers.sort()
+
+// // Use the 'compare function' - sorts by whole number value
+// val = numbers.sort(function(x,y){
+//   return x-y
+// })
+
+// // Reverse Sort
+// val = numbers.sort(function(x,y){
+//   return y-x
+// })
+
+// // Find 
+// function under50(num){
+//   return num < 50
+// }
+
+// function over50(num){
+//   return num > 50
+// }
+
+// val = numbers.find(over50)
+
+// console.log(numbers)
+// console.log(val)
+
+
+// OBJECT LITERAL Notes
+
+const person = {
+  firstName: 'Steve',
+  lastName: 'Smith',
+  age: 36,
+  email: 'steve@aol.com',
+  hobbies: ['music', 'sports'],
+  address: {
+    city: 'Miami',
+    state: 'FL'
+  },
+  getBirthYear: function(){
+    return 2017 - this.age
+  }
+}
 
 let val 
 
-// Get array length
-val = numbers.length
-// Check if is array
-val = Array.isArray(numbers)
-// Get single value
-val = numbers[3]
-val = numbers[0]
-// Insert into Array
-numbers[2] = 100
-// Find index of value
-val = numbers.indexOf(36)
+val = person
+// Get specific value
+val = person.firstName
+val = person['lastName']
+val = person.age
+val = person.hobbies[1]
+val = person.address.state
+val = person.address['city']
+val = person.getBirthYear()
 
-// MUTATING ARRAYS
-// Add on to end
-numbers.push(250)
-// Add to front
-numbers.unshift(120)
-// Take off from end
-numbers.pop()
-// Take off from front
-numbers.shift()
-// Splice values
-numbers.splice(1,3)
-// Reverse
-numbers.reverse()
-
-// Concatenate array
-val = numbers.concat(numbers2)
-
-// Sorting arrays
-val = fruit.sort()
-// sorting numbers sorts by first digit only
-val = numbers.sort()
-
-// Use the 'compare function' - sorts by whole number value
-val = numbers.sort(function(x,y){
-  return x-y
-})
-
-// Reverse Sort
-val = numbers.sort(function(x,y){
-  return y-x
-})
-
-// Find 
-function under50(num){
-  return num < 50
-}
-
-function over50(num){
-  return num > 50
-}
-
-val = numbers.find(over50)
-
-console.log(numbers)
 console.log(val)
+
+const people = [
+  {name: 'John', age: 30},
+  {name: 'Mike', age: 23},
+  {name: 'Nancy', age: 40}
+]
+
+for(let i = 0; i < people.length; i++){
+  console.log(people[i].name)
+}

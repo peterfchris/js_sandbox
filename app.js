2e946eb36390e12a0a30ba4dc87902deeb1a7c9d
peterfchris <peter.f.chris@gmail.com>
2020-11-19 08:05:42 -0700

    Switch notes

--- app.js
+++ app.js
@@ -415,87 +415,132 @@
 
 // If Statements & Comparison Operators
 
-const id = 100
+// const id = 100
 
-// EQUALS VALUE
-if(id == 101){
-  console.log('CORRECT')
-} else {
-  console.log('INCORRECT')
-}
+// // EQUALS VALUE
+// if(id == 101){
+//   console.log('CORRECT')
+// } else {
+//   console.log('INCORRECT')
+// }
 
-// NOT EQUAL TO VALUE
-if(id != 101){
-  console.log('CORRECT')
-} else {
-  console.log('INCORRECT')
-}
+// // NOT EQUAL TO VALUE
+// if(id != 101){
+//   console.log('CORRECT')
+// } else {
+//   console.log('INCORRECT')
+// }
 
-// EQUALS VALUE & TYPE
-if(id === 101){
-  console.log('CORRECT')
-} else {
-  console.log('INCORRECT')
-}
+// // EQUALS VALUE & TYPE
+// if(id === 101){
+//   console.log('CORRECT')
+// } else {
+//   console.log('INCORRECT')
+// }
 
-// NOT EQUAL TO VALUE & TYPE
-if(id !== 101){
-  console.log('CORRECT')
-} else {
-  console.log('INCORRECT')
-}
+// // NOT EQUAL TO VALUE & TYPE
+// if(id !== 101){
+//   console.log('CORRECT')
+// } else {
+//   console.log('INCORRECT')
+// }
 
-// TO TEST IF UNDEFINED
-if(typeof id !== undefined){
-  console.log(`The ID is ${id}`)
-} else {
-  console.log('NO ID')
-}
+// // TO TEST IF UNDEFINED
+// if(typeof id !== undefined){
+//   console.log(`The ID is ${id}`)
+// } else {
+//   console.log('NO ID')
+// }
 
-// GREATER OR LESS THAN
-if(id <= 100){
-  console.log('CORRECT')
-} else {
-  console.log('INCORRECT')
-}
+// // GREATER OR LESS THAN
+// if(id <= 100){
+//   console.log('CORRECT')
+// } else {
+//   console.log('INCORRECT')
+// }
 
-// IF ELSE
+// // IF ELSE
+// const color = 'yellow'
+
+// if(color === 'red'){
+//   console.log('Color is red')
+// } else if(color === 'blue'){
+//   console.log('Color is blue')
+// } else {
+//   console.log('Color is yellow')
+// }
+
+// // LOGICAL OPERATORS
+
+// const name = 'Steve'
+// const age = 20
+
+// // AND &&
+// if(age > 0 && age < 12){
+//   console.log(`${name} is a child`)
+// } else if(age >= 13 && age <= 19){
+//   console.log(`${name} is a teenager`)
+// } else {
+//   console.log(`${name} is an adult`)
+// }
+
+// // OR ||
+// if(age < 16 || age > 65){
+//   console.log(`${name} cannot run in race`)
+// } else {
+//   console.log(`${name} is registered for the race`)
+// }
+
+// // TERNARY OPERATOR
+// console.log(id === 100 ? 'CORRECT' : 'INCORRECT')
+
+// // WITHOUT BRACES
+// if(id === 100)
+//   console.log('CORRECT')
+// else
+//   console.log('INCORRECT')
+
+
+// SWITCHES Notes
+
 const color = 'yellow'
 
-if(color === 'red'){
-  console.log('Color is red')
-} else if(color === 'blue'){
-  console.log('Color is blue')
-} else {
-  console.log('Color is yellow')
+switch(color){
+  case 'red':
+    console.log(('Color is red'))
+    break
+  case 'blue':
+    console.log(('Color is blue'))
+    break
+  default:
+    console.log(('Color is not red or blue'))
+    break
 }
 
-// LOGICAL OPERATORS
+let day
 
-const name = 'Steve'
-const age = 20
-
-// AND &&
-if(age > 0 && age < 12){
-  console.log(`${name} is a child`)
-} else if(age >= 13 && age <= 19){
-  console.log(`${name} is a teenager`)
-} else {
-  console.log(`${name} is an adult`)
+switch(new Date().getDay()){
+  case 0:
+    day = 'Sunday'
+    break
+  case 1:
+    day = 'Monday'
+    break
+  case 2:
+    day = 'Tuesday'
+    break
+  case 3:
+    day = 'Wednesday'
+    break
+  case 4:
+    day = 'Thursday'
+    break
+  case 5:
+    day = 'Friday'
+    break
+  case 6:
+    day = 'Saturday'
+    break
 }
 
-// OR ||
-if(age < 16 || age > 65){
-  console.log(`${name} cannot run in race`)
-} else {
-  console.log(`${name} is registered for the race`)
-}
-
-// TERNARY OPERATOR
-console.log(id === 100 ? 'CORRECT' : 'INCORRECT')
-
-// WITHOUT BRACES
-if(id === 100)
-  console.log('CORRECT')
-else
-  console.log('INCORRECT')
+console.log(`Today is ${day}`)

cd8f0d1b65be95e93118c71d49cec13e4180ea63
peterfchris <peter.f.chris@gmail.com>
2020-12-04 08:26:35 -0700

    Keyboard events

--- events.js
+++ events.js
@@ -59,14 +59,58 @@ const heading = document.querySelector('h5')
 // card.addEventListener('mouseover', runEvent)
 // // Mouseout
 // card.addEventListener('mouseout', runEvent)
-// Mousemove
-card.addEventListener('mousemove', runEvent)
+// // Mousemove
+// card.addEventListener('mousemove', runEvent)
 
-// Event handler
-function runEvent(e){
+// // Event handler
+// function runEvent(e){
+//   console.log(`EVENT TYPE: ${e.type}`)
+
+//   heading.textContent = `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`
+
+  // document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`
+// }
+
+
+// Keyboard & Input Events
+const form = document.querySelector('form')
+const taskInput = document.getElementById('task')
+const heading = document.querySelector('h5')
+const select = document.querySelector('select')
+
+// Clear Input
+taskInput.value = ''
+
+form.addEventListener('submit', runEvent)
+
+// Keydown
+// taskInput.addEventListener('keydown', runEvent)
+// Keyup
+taskInput.addEventListener('keyup', runEvent)
+// Keypress
+taskInput.addEventListener('keypress', runEvent)
+// Focus
+taskInput.addEventListener('focus', runEvent)
+// Blur
+taskInput.addEventListener('blur', runEvent)
+// Cut
+taskInput.addEventListener('cut', runEvent)
+// Paste
+taskInput.addEventListener('paste', runEvent)
+// Input
+taskInput.addEventListener('input', runEvent)
+// Change
+select.addEventListener('change', runEvent)
+
+function runEvent(e) {
   console.log(`EVENT TYPE: ${e.type}`)
 
-  heading.textContent = `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`
+  // console.log(e.target.value)
 
-  document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`
+  // heading.innerText = e.target.value
+
+  // // Get input value
+  // console.log(taskInput.value)
+
+  // e.preventDefault()
 }

e8115d1868ccea86e3d40e0fa2077939e555f323
peterfchris <peter.f.chris@gmail.com>
2020-12-08 08:18:33 -0700

    Local and Session storage notes

--- events.js
+++ events.js
@@ -136,15 +136,15 @@
 
 // delItem.addEventListener('click', deleteItem)
 
-document.body.addEventListener('click'. deleteItem)
+// document.body.addEventListener('click'. deleteItem)
 
-function deleteItem(e){
-  // if(e.target.parentElement.className === 'delete-item secondary-content'){
-  //   console.log('delete item')
-  // }
+// function deleteItem(e){
+//   // if(e.target.parentElement.className === 'delete-item secondary-content'){
+//   //   console.log('delete item')
+//   // }
 
-  if(e.target.parentElement.classList.contains('delete-item')){
-    console.log('delete item')
-    e.target.parentElement.parentElement.remove()
-  }
-}
+//   if(e.target.parentElement.classList.contains('delete-item')){
+//     console.log('delete item')
+//     e.target.parentElement.parentElement.remove()
+//   }
+// }
